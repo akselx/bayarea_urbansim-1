a9c0bf36db2e152e7c345d5e8f4313edc344691b
yuqi <ywang@bayareametro.gov>
2020-06-03 20:55:19 -0700

    Update test_datasources.py

--- baus/tests/test_datasources.py
+++ baus/tests/test_datasources.py
@@ -39,10 +39,44 @@ def test_inclusionary_housing_settings():
             ]
         }]
     }
+    inclusionary_housing_settings_fr2 = {
+        "foo": [{
+            "amount": .2,
+            "values": [
+                "Berkeley",
+                "Oakland"
+            ]
+        }, {
+            "amount": .1,
+            "values": [
+                "San Francisco"
+            ]
+        }]
+    }
+    inclusionary_housing_settings_d_b = {
+        "foo": [{
+            "amount": .2,
+            "values": [
+                "Berkeley",
+                "Oakland"
+            ]
+        }, {
+            "amount": .1,
+            "values": [
+                "San Francisco"
+            ]
+        }]
+    }
     out = datasources.inclusionary_housing_settings({
         "inclusionary_housing_settings": inclusionary_housing_settings
     }, "foo")
+    out_fr2 = datasources.inclusionary_housing_settings({
+        "inclusionary_housing_settings": inclusionary_housing_settings_fr2
+    }, "foo")
+    out_d_b = datasources.inclusionary_housing_settings({
+        "inclusionary_housing_settings": inclusionary_housing_settings_d_b
+    }, "foo")
 
-    assert out["Berkeley"] == .2
-    assert out["Oakland"] == .2
-    assert out["San Francisco"] == .1
+    assert ((out["Berkeley"] == .2) or (out_fr2["Berkeley"] == .2) or (out_d_b["Berkeley"] == .2))
+    assert ((out["Oakland"] == .2) or (out_fr2["Oakland"] == .2) or (out_d_b["Oakland"] == .2))
+    assert ((out["San Francisco"] == .1) or (out_fr2["San Francisco"] == .1) or (out_d_b["San Francisco"] == .1))
